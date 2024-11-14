339f77529be1fb4497e0256e00d9307f0242047e
iLikeGrape <135122587+iLikeGrape@users.noreply.github.com>
2024-11-14 20:05:02 +0000

    Tic Tac Toe

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cells = document.querySelectorAll(".cell");
 const statusText = document.querySelector("#statusText");
-const restartButton = document.querySelector("#restartButton");
+const restartBtn = document.querySelector("#restartBtn");
 const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -15,15 +15,15 @@ let options = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let running = false;
 
-initializeGame() 
+initializeGame()
 
 function initializeGame(){
-    cells.forEach(cell => cell.addEventListener("click", cellClicked));
-    restartButton.addEventListener("click", restartGame);
+    cells.forEach(cell => cell.addEventListener("click", cellClicked))
+    restartBtn.addEventListener("click", restartGame)
     statusText.textContent = `${currentPlayer}'s turn`;
-    running = true
-} 
-function cellClicked(cellIndex){
+    running = true;
+}
+function cellClicked(){
     const cellIndex = this.getAttribute("cellIndex");
 
     if(options[cellIndex] != "" || !running){
@@ -31,53 +31,43 @@ function cellClicked(cellIndex){
     }
 
     updateCell(this, cellIndex);
+    changePlayer();
     checkWinner();
 }
 function updateCell(cell, index){
     options[index] = currentPlayer;
     cell.textContent = currentPlayer;
-
 }
 function changePlayer(){
     currentPlayer = (currentPlayer == "X") ? "O" : "X";
     statusText.textContent = `${currentPlayer}'s turn`;
-    console.log("changedPlayer");
-
 }
 function checkWinner(){
     let roundWon = false;
-    console.log("checkWinner");
 
     for(let i = 0; i < winConditions.length; i++){
         const condition = winConditions[i];
         const cellA = options[condition[0]];
         const cellB = options[condition[1]];
         const cellC = options[condition[2]];
-        if(cellA == "" || cellB == "" || cellC == "" ){
-            console.log("BlankSquareDetected");
-            continue
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
         }
-        if(cellA == cellB && cellB == cellC ){
-            console.log("allEqual")
-            if(cellA == "X" || cellA == "O")
+        if(cellA == cellB && cellB == cellC){
             roundWon = true;
-            console.log("roundWonSet2True");
             break;
         }
     }
-
     if(roundWon){
-        statusText.textContent = `${currentPlayer} wins!`
-        running = true;
-        console.log("win");
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
     }
-    else if (!options.includes("")){
-        statusText.textContent = `Draw!`
-        running = true;
-        console.log("tie");
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
     }
     else{
-        console.log("gameNotOver");
         changePlayer();
     }
 }
@@ -86,10 +76,5 @@ function restartGame(){
     options = ["", "", "", "", "", "", "", "", ""];
     statusText.textContent = `${currentPlayer}'s turn`;
     cells.forEach(cell => cell.textContent = "");
-    cells.forEach(console.log(`${cell.textContent}`));
-    console.log(`${options}`)
-    
-    initializeGame();
-
-    
-};
+    running = true;
+}
